273d02fd5e3d2a53632dd7836831d1f7e6b47406
RuiFPB <rpinhobarbosa@gmail.com>
2024-04-29 14:22:15 +0100

    Updated GCC and BINUTILS Also added the capability to choose wich version of gcc or binutils to use using environment variables Removed libcloog from dependencies as it no longer exists on the ubuntu apt repo

--- cross-compiler.sh
+++ cross-compiler.sh
@@ -5,7 +5,18 @@ if [ "$#" -ne 1 ]; then
   exit 1
 fi
 
-sudo apt install libgmp3-dev libmpfr-dev libisl-dev libcloog-isl-dev libmpc-dev texinfo -y
+if [ -z "${BINUTILS}" ]; then
+	BINUTILS="binutils-2.38"
+fi
+echo "Using ${BINUTILS}"
+
+if [ -z "${GCC}" ]; then
+	GCC="gcc-11.4.0"
+fi
+echo "Using ${GCC}"
+
+echo "Installing dependencies... (root might be needed)"
+sudo apt install libgmp3-dev libmpfr-dev libisl-dev libmpc-dev texinfo -y
 
 cd "$(dirname "$0")"
 
@@ -17,11 +28,11 @@ mkdir src
 
 cd src
 
-wget https://ftp.gnu.org/gnu/binutils/binutils-2.33.1.tar.xz
-wget ftp://ftp.gnu.org/gnu/gcc/gcc-9.2.0/gcc-9.2.0.tar.xz
+wget https://ftp.gnu.org/gnu/binutils/$BINUTILS.tar.xz
+wget https://ftp.gnu.org/gnu/gcc/$GCC/$GCC.tar.xz
 
-tar -xvf binutils-2.33.1.tar.xz
-tar -xvf gcc-9.2.0.tar.xz
+tar -xvf $BINUTILS.tar.xz
+tar -xvf $GCC.tar.xz
 
 export PREFIX="$(pwd)/../path/"
 export TARGET=$1
@@ -29,14 +40,14 @@ export PATH="$PREFIX/bin:$PATH"
 
 mkdir build-binutils
 cd build-binutils
-../binutils-2.33.1/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+../$BINUTILS/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
 make -j $(nproc)
 make install
 
 cd ..
 mkdir build-gcc
 cd build-gcc
-../gcc-9.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
+../$GCC/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
 make -j $(nproc) all-gcc
 make -j $(nproc) all-target-libgcc
 make install-gcc
